c660af2b96c78b597dd88e6b3e1454bf4a8e4d2a
Carlos Henrique da Silva Moreira <chmoreira@mag.com.br>
2025-11-05 21:39:12 -0300

    Ajusta endpoint DELETE /user
    Nessa implementação foi alterado do design original proposto, retornando 204 (No Content) e tornando o endpoint idempotente

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Users/UsersController.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Users/UsersController.cs
@@ -112,10 +112,6 @@ public class UsersController : BaseController
         var command = _mapper.Map<DeleteUserCommand>(request.Id);
         await _mediator.Send(command, cancellationToken);
 
-        return Ok(new ApiResponse
-        {
-            Success = true,
-            Message = "User deleted successfully"
-        });
+        return NoContent();
     }
 }
